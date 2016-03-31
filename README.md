47c288f708d6f961cb1ae05b409ee3a795249b38
Robert DeVore <deviodigital@gmail.com>
2016-03-31 15:17:42 -0400

    initial creation

---
+++ README.md
@@ -0,0 +1,5 @@
+# twentysixteen child theme
+
+Currently being built and tested at my [personal blog](http://www.robertdevore.com).
+
+More details and final prep will be put into the code before the official release.
---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+    wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' );
+    wp_enqueue_style( 'merriweather', 'https://fonts.googleapis.com/css?family=Merriweather:400,700' );
+    wp_enqueue_style( 'lato', 'https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' );
+}
+
+function robertdevore_credits() { ?>
+	&copy; 2015-16 <span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+	Powered by <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>">WordPress</a> and <a href="<?php echo esc_url( __( 'http://deviodigital.com/', 'twentysixteen' ) ); ?>"><?php printf( __( '%s', 'twentysixteen' ), 'Devio Digital' ); ?></a>
+<?php }
+add_action('twentysixteen_credits','robertdevore_credits');
